540f881a1440659cb62994355f323521a4c4d93d
mARIFs2000 <mas12thjuly2000@gmail.com>
2024-12-03 13:33:20 +0530

    Add files via upload

--- Arif_Python/GUI.py
+++ Arif_Python/GUI.py
@@ -1,14 +1,38 @@
-from tkinter import* 
-add_window = Tk() 
-add_window.title("ArifWeb")
-fist_window = Label(add_window,text="We are in Spectrum",bg="silver",fg="black",font=(("FIXEDSYS","35")))
-fist_window.pack()
-path1 =PhotoImage(file="C:/Users/Python/Downloads/Spectrum Building.png")
-Lbl_image = Label(add_window,image=path1,width=200,height=300)
-Lbl_image.place(x=950,y=58)
+# # FACEBOOK LOGIN PAGE
+# from tkinter import*
 
-path2 =PhotoImage(file="C:\Users\Python\Pictures\Camera Roll\Benz png.jpg")
-lbl2_image = Label(add_window,image=path2,width=400,height=250)
-lbl2_image.place(x=150,y=150)
-add_window.geometry("1280x720")
-add_window.mainloop()
+# flowering = Tk()
+# flowering.title("FB Login Page")
+# flower =Label(flowering,text = "facebook",bg="white",fg="blue",font=('helvetica',"40"))
+# flower.place(x=530,y=250)
+# def clicked():
+#     print("clicked")
+
+# flower1 =Label(flowering,text= "User Name:-",font= ('courier new',"10") ) 
+# flower1.place(x=480,y=340)   
+
+# flower2 =Label(flowering,text= "Password:-",font= ('courier new',"10"))
+# flower2.place(x=480,y=400)
+# btn= Button(flowering, text="login", command = clicked)
+# btn.place(x=620,y=460)
+
+# flower_p=PhotoImage(file="C:/Users/Python/Downloads/facebook login(1).png")
+# flower_p1=Label(flowering, image=flower_p,width=250,height=250)
+# flower_p1.pack()
+# entry =Entry(flowering)
+# entry.place(x=575,y=340)
+# entry1 =Entry(flowering)
+# entry1.place(x=575,y=400)
+
+# flowering.geometry('1280x720') 
+# flowering.mainloop()
+
+# CALCULATOR 
+from tkinter import*
+
+calculator = Tk()
+calculator.title("Calculator")
+
+
+
+
